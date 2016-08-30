6ddbd85dc169d05d6bd3d09dc54a82e14cdfbbbf
yuebaiXu <xyb1114@gmail.com>
2016-08-30 16:32:44 +0200

    index

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,9 @@ var browserSync = require('browser-sync').create();
 gulp.task('server', function() {
     browserSync.init({
         server: {
-            baseDir: "./2-header/header-dropdown/"
+            baseDir: "./"
         }
     });
 
-    gulp.watch("./2-header/header-dropdown/*.html").on('change', browserSync.reload);	
+    gulp.watch("./*.html").on('change', browserSync.reload);	
 });
