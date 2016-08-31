db3345be7cb38de7db901f31e30df91236c4805c
yuebaiXu <xyb1114@gmail.com>
2016-08-31 13:53:51 +0200

    collapse

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,9 @@ var browserSync = require('browser-sync').create();
 gulp.task('server', function() {
     browserSync.init({
         server: {
-            baseDir: "./1-sidebar/sidebar-animation"
+            baseDir: "./1-sidebar/sidebar-collapse"
         }
     });
 
-    gulp.watch("./1-sidebar/sidebar-animation/*.html").on('change', browserSync.reload);	
+    gulp.watch("./1-sidebar/sidebar-collapse/*.html").on('change', browserSync.reload);	
 });
